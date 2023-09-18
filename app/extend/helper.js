bbb6c1590e2aa36814e5ed55ff88be1d63205f76
shanzemin <shanzemin@qq.com>
2023-09-18 16:25:32 +0800

    🎉 init: 初始化项目

---
+++ app/extend/helper.js
@@ -0,0 +1,7 @@
+'use strict'
+
+module.exports = {
+  testPlugin() {
+    console.log('current is test plugin')
+  }
+}
---
+++ config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * egg-hello default config
+ * @member Config#hello
+ * @property {String} SOME_KEY - some description
+ */
+exports.hello = {
+
+};
